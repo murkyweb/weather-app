29131cb53d5a9c7809786330c600385e7fa8b941
Alexandru <murkyweb@protonmail.com>
2022-06-28 19:55:19 +0300

    Added error message when location is not found

--- src/domstuff.js
+++ src/domstuff.js
@@ -9,39 +9,52 @@ const DOMstuff = (() => {
   };
 
   async function populate(zone) {
-    const data = await Weather.getWeather(`${zone}`);
+    try {
+      const errorMessage = document.querySelector(".error");
+      errorMessage.textContent = "";
+      errorMessage.className = "error";
 
-    const weather = document.querySelector(".weather");
-    const location = document.querySelector(".location");
-    const temp = document.querySelector(".temp");
-    const weatherCondition = document.querySelector(".weather-condition");
-    const humidity = document.querySelector(".humidity");
-    const pressure = document.querySelector(".pressure");
-    const minTemp = document.querySelector(".min-temp");
-    const maxTemp = document.querySelector(".max-temp");
-    const time = document.querySelector(".second-row");
-    const weatherIcon = document.querySelector(".weather-img");
-    const weatherDescription = document.querySelector(".weather-description");
+      const data = await Weather.getWeather(`${zone}`);
 
-    location.innerHTML = `<span>${data.name}</span><span>${data.country}</span>`;
-    temp.innerHTML = `<span>${Conversion.kelvinToCelsius(data.temp)} °C</span>`;
-    weatherCondition.innerHTML = `<span>${data.weatherCondition}</span>`;
-    humidity.innerHTML = `<span>Humidity </span><span>${data.humidity} %</span>`;
-    pressure.innerHTML = `<span>Pressure </span><span>${data.pressure} hPa</span>`;
-    minTemp.innerHTML = `<span>Min. Temperature </span><span>${Conversion.kelvinToCelsius(
-      data.minTemp
-    )} °C</span>`;
-    maxTemp.innerHTML = `<span>Max. Temperature </span><span>${Conversion.kelvinToCelsius(
-      data.maxTemp
-    )} °C</span>`;
-    time.innerHTML = `<span>${Conversion.unixTimestampToDate(
-      data.time
-    )} </span>`;
-    weatherIcon.style.backgroundImage = `url("http://openweathermap.org/img/wn/${data.weatherIcon}@2x.png")`;
-    weatherIcon.style.width = "80px";
-    weatherIcon.style.height = "60px";
-    weatherDescription.innerHTML = `<span>${data.weatherDescription}</span>`;
-    weather.className = "weather show";
+      const weather = document.querySelector(".weather");
+      const location = document.querySelector(".location");
+      const temp = document.querySelector(".temp");
+      const weatherCondition = document.querySelector(".weather-condition");
+      const humidity = document.querySelector(".humidity");
+      const pressure = document.querySelector(".pressure");
+      const minTemp = document.querySelector(".min-temp");
+      const maxTemp = document.querySelector(".max-temp");
+      const time = document.querySelector(".second-row");
+      const weatherIcon = document.querySelector(".weather-img");
+      const weatherDescription = document.querySelector(".weather-description");
+
+      location.innerHTML = `<span>${data.name}</span><span>${data.country}</span>`;
+      temp.innerHTML = `<span>${Conversion.kelvinToCelsius(
+        data.temp
+      )} °C</span>`;
+      weatherCondition.innerHTML = `<span>${data.weatherCondition}</span>`;
+      humidity.innerHTML = `<span>Humidity </span><span>${data.humidity} %</span>`;
+      pressure.innerHTML = `<span>Pressure </span><span>${data.pressure} hPa</span>`;
+      minTemp.innerHTML = `<span>Min. Temperature </span><span>${Conversion.kelvinToCelsius(
+        data.minTemp
+      )} °C</span>`;
+      maxTemp.innerHTML = `<span>Max. Temperature </span><span>${Conversion.kelvinToCelsius(
+        data.maxTemp
+      )} °C</span>`;
+      time.innerHTML = `<span>${Conversion.unixTimestampToDate(
+        data.time
+      )} </span>`;
+      weatherIcon.style.backgroundImage = `url("http://openweathermap.org/img/wn/${data.weatherIcon}@2x.png")`;
+      weatherIcon.style.width = "80px";
+      weatherIcon.style.height = "60px";
+      weatherDescription.innerHTML = `<span>${data.weatherDescription}</span>`;
+      weather.className = "weather show";
+    } catch (error) {
+      const errorMessage = document.querySelector(".error");
+      errorMessage.textContent = "Location not found";
+      errorMessage.className = "error active";
+      console.log("error");
+    }
   }
 
   return { populate, resetInput };
